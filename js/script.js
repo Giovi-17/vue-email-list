19f8406840bff5c0ffc3227e90aa69ccd3865533
Giovi-17 <giovanni.nuzzo17@gmail.com>
2021-12-15 20:55:48 +0100

    Vue Email List + BONUS

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/*
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email 
+e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente 
+quando sono stati tutti generati.
+*/
+
+Vue.config.devtools = true;
+
+const app = new Vue(
+
+    {
+
+        el: "#root",
+        data: {
+
+            emailApi: [],
+
+        },
+        methods: {
+
+            emailGenerator: function(){
+
+                for( let i = 0; i < 10; i++ ){
+
+                    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then( (response) => {
+
+                    this.emailApi.push(response.data.response);
+                    /* console.log(this.emailApi); */
+
+                } );
+
+                }
+
+            }
+
+        },
+        created: function() {
+
+            this.emailGenerator();
+
+        }
+
+    }
+
+);
